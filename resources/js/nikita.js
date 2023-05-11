c0f21453ddcbf150f073e1ba737b22349c05a646
auramel <auramel@yandex.ru>
2023-05-11 15:43:20 +0500

    init

--- resources/js/nikita.js
+++ resources/js/nikita.js
@@ -2,6 +2,12 @@ import fetch from 'node-fetch';
 import fs from 'file-system';
 
 console.log('running scripts...');
-
+await sleep(10000);
 const result = await fetch('https://app.chatboost.io/logs');
 fs.writeFile('TEST.txt', result.status.toString());
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
